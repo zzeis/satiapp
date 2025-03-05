ce0e73531135b3225ec556ed257e9cbce00ca2fd
zzeis <moisesmonteiro.stark@gmail.com>
2025-03-05 14:03:49 -0300

    fix: add new relations for termoEntrega and equipamentosReponsavel

--- app/Models/Pessoa.php
+++ app/Models/Pessoa.php
@@ -5,18 +5,21 @@ namespace App\Models;
 use App\Models\Equipamento;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pessoa extends Model
 {
-    use HasUlids, SoftDeletes;
+    use  SoftDeletes, HasUuids;
 
+    protected $keyType = 'string'; 
     protected $fillable = [
         'nome',
         'cpf',
         'cargo',
+        
         'secretaria_id'
     ];
 
@@ -28,5 +31,29 @@ class Pessoa extends Model
     public function equipamentos()
     {
         return $this->hasMany(Equipamento::class, 'responsavel_id');
+    }
+
+     
+     // Relacionamento com TermoEntrega
+     public function termosEntrega()
+     {
+         return $this->hasMany(TermoEntrega::class, 'responsavel_id');
+     }
+     
+     // Relacionamento com Equipamentos (onde é responsável)
+     public function equipamentosResponsavel()
+     {
+         return $this->hasMany(Equipamento::class, 'responsavel_id');
+     }
+
+      /**
+     * Mutator para o campo "modelo".
+     * Converte o valor para maiúsculas antes de salvar no banco de dados.
+     *
+     * @param string $value
+     */
+    public function setNomeAttribute($value)
+    {
+        $this->attributes['nome'] = strtoupper($value);
     }
 }
