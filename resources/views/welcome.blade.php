a60670e16dbba74b059bb457b1bc9749e6d35e6a
zzeis <moisesmonteiro.stark@gmail.com>
2025-03-05 15:45:17 -0300

    feat: new design add logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,26 +6,34 @@
     <title>Satiapp - Bem-vindo</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body class="bg-gray-100 flex items-center justify-center h-screen">
-    <div class="bg-white p-8 rounded-lg shadow-lg text-center">
-        <h1 class="text-3xl font-bold mb-4"> SatiaAPP</h1>
-        <p class="text-gray-600 mb-8">Gestão Técnologia e Informação</p>
+<body class="bg-gray-100 flex items-center justify-center min-h-screen p-4">
+    <div class="bg-white rounded-lg shadow-lg flex flex-col md:flex-row overflow-hidden max-w-4xl w-full">
+        <!-- Bloco do Logo -->
+        <div class="bg-gray-300 p-8 flex items-center justify-center md:w-1/2">
+            <img src="{{ asset('images/LogoSATI.png') }}" alt="Logo" class="h-40 w-auto">
+        </div>
 
-        <div class="space-y-4">
-            <!-- Botão de Login -->
-            <a href="{{ route('login') }}" class="block w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300">
-                Login
-            </a>
+        <!-- Bloco dos Botões -->
+        <div class="p-8 flex flex-col items-center justify-center md:w-1/2">
+            <h1 class="text-3xl font-bold mb-4">SatiaAPP</h1>
+            <p class="text-gray-600 mb-8">Gestão, Tecnologia e Informação</p>
 
-            <!-- Botão de Registro -->
-            <a href="{{ route('register') }}" class="block w-full bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition duration-300">
-                Registrar
-            </a>
+            <div class="space-y-4 w-full">
+                <!-- Botão de Login -->
+                <a href="{{ route('login') }}" class="block w-full bg-gray-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 text-center">
+                    Login
+                </a>
 
-            <!-- Link para ir direto ao Login -->
-            <p class="text-gray-600 mt-4">
-                Já tem uma conta? <a href="{{ route('login') }}" class="text-blue-500 hover:underline">Faça login aqui</a>.
-            </p>
+                <!-- Botão de Registro -->
+                <a href="{{ route('register') }}" class="block w-full bg-gray-500 text-white py-2 px-4 rounded hover:bg-green-600 transition duration-300 text-center">
+                    Registrar
+                </a>
+
+                <!-- Link para ir direto ao Login -->
+                <p class="text-gray-600 mt-4 text-center">
+                    Já tem uma conta? <a href="{{ route('login') }}" class="text-blue-500 hover:underline">Faça login aqui</a>.
+                </p>
+            </div>
         </div>
     </div>
 </body>
