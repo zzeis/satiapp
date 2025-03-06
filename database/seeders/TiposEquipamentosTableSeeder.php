d7f53bd69275f2d182a304f371a402d053bf18c4
zzeis <moisesmonteiro.stark@gmail.com>
2025-03-06 16:09:57 -0300

    fix: new info for seed

--- database/seeders/TiposEquipamentosTableSeeder.php
+++ database/seeders/TiposEquipamentosTableSeeder.php
@@ -10,14 +10,17 @@ class TiposEquipamentosTableSeeder extends Seeder
     public function run(): void
     {
         $tipos_equipamentos = [
-            ['id' => 1, 'nome' => 'DESKTOP'],
-            ['id' => 2, 'nome' => 'MONITOR'],
+            ['id' => 1, 'nome' => 'DESKTOP - 1 CABO A/C'],
+            ['id' => 2, 'nome' => 'MONITOR - 1 CABO A/C - 1 CABO VGA'],
             ['id' => 3, 'nome' => 'ESTABILIZADOR'],
             ['id' => 4, 'nome' => 'MOUSE'],
             ['id' => 5, 'nome' => 'TECLADO'],
-            ['id' => 6, 'nome' => 'IMPRESSORA'],
-            ['id' => 7, 'nome' => 'NOTEBOOK'],
-            ['id' => 8, 'nome' => 'TABLET'],
+            ['id' => 6, 'nome' => 'IMPRESSORA - 1 CABO A/C'],
+            ['id' => 7, 'nome' => 'NOTEBOOK - 1 FONTE'],
+            ['id' => 8, 'nome' => 'TABLET - 1 CARREGADOR'],
+            ['id' => 9, 'nome' => 'MONITOR - 1 FONTE - 1 CABO VGA'],
+            ['id' => 10, 'nome' => 'MONITOR - 1 FONTE - 1 CABO HDMI'],
+            ['id' => 11, 'nome' => 'MONITOR - 1 CABO A/C - 1 CABO HDMI'],
         ];
 
         // Limpa a tabela antes de inserir
