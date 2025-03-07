9d8310b0540574e92052f4f8c990791982fa9a99
zzeis <moisesmonteiro.stark@gmail.com>
2025-03-07 09:38:05 -0300

    fix: remove ti.css

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
             input: [
                 "resources/css/app.css",
                 "resources/js/app.js",
-                "resources/css/ti.css",
+               
             ],
             refresh: true,
         }),
