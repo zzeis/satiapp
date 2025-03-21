b70bb6a71b1a3f3d4b874728db5f4f0b10b8d965
zzeis <moisesmonteiro.stark@gmail.com>
2025-03-21 13:59:35 -0300

    feat: add info at model Logs

--- app/Models/Logs.php
+++ app/Models/Logs.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Logs extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'acao',
+        'detalhes',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
